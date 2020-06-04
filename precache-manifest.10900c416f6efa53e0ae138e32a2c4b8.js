428b40690963802428be13e02ddf2c71eb9f22e8
Jaya-272009 <jayagaur911@gmail.com>
2020-06-04 15:35:40 +0530

    Updates

---
+++ precache-manifest.10900c416f6efa53e0ae138e32a2c4b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c82960e45ea8a5539109f6d7f84cc41",
+    "url": "/stripepayment/index.html"
+  },
+  {
+    "revision": "8d8d4fbd4b52d19a3e5d",
+    "url": "/stripepayment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a1815bdd375c35107716",
+    "url": "/stripepayment/static/js/2.9aaf1cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripepayment/static/js/2.9aaf1cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8d4fbd4b52d19a3e5d",
+    "url": "/stripepayment/static/js/main.814a4253.chunk.js"
+  },
+  {
+    "revision": "09dbe6405f64c398ad32",
+    "url": "/stripepayment/static/js/runtime-main.486fb897.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stripepayment/static/media/logo.5d5d9eef.svg"
+  }
+]);
